c922299916c8834f9fb9b183e0b72133096286a9
Sipxbuild <sipxbuild@telecats.nl>
2013-08-14 13:38:25 +0200

    SIPXECS-1068: Changes to fix build issues. Newest sipXhotdesking

---
+++ mak/telecats-build.sipxecs.mk
@@ -0,0 +1,14 @@
+telecats_build = \
+  sipXacccode \
+  sipXcustomCallerId \
+  sipXviewer \
+  sipXimbot \
+  sipXrest \
+  sipXcallController \
+  sipXcdrLog \
+  sipXevent \
+  sipXrecording \
+  sipXsbc \
+  sipXhomer \
+  sipXhotdesking \
+  sipXaastra
